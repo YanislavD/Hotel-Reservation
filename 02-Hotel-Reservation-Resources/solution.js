ca494f5a91ca0aad25649f3db18e9b78446aceba
RaykoRadev <christmas898@gmail.com>
2026-05-22 15:40:36 +0200

    adding 2 missing }

--- 02-Hotel-Reservation-Resources/solution.js
+++ 02-Hotel-Reservation-Resources/solution.js
@@ -95,57 +95,65 @@ function searchFormData(e) {
     }
 }
 
-document.querySelector('#search-back-btn').addEventListener('click', (e) => fillSearchForm(e));
+document
+    .querySelector("#search-back-btn")
+    .addEventListener("click", (e) => fillSearchForm(e));
 
 function fillSearchForm(e) {
     e.preventDefault();
-    changeContent('search-form-content');
-    document.querySelector('#check-in').value = reservation.startDate;
-    document.querySelector('#check-out').value = reservation.endDate;
-    document.querySelector('#people').value = reservation.guestsCount;
+    changeContent("search-form-content");
+    document.querySelector("#check-in").value = reservation.startDate;
+    document.querySelector("#check-out").value = reservation.endDate;
+    document.querySelector("#people").value = reservation.guestsCount;
 }
 
-
-document.querySelectorAll('.room-type').forEach(room => {
-    room.addEventListener("click", (e) => selectRoomType(e))
+document.querySelectorAll(".room-type").forEach((room) => {
+    room.addEventListener("click", (e) => selectRoomType(e));
 });
 
 function selectRoomType(e) {
     let myTarget = undefined;
     e.preventDefault;
-    if (e.target.querySelector('img') != null) {
+    if (e.target.querySelector("img") != null) {
         myTarget = e.target;
     } else {
         myTarget = e.target.parentElement;
     }
-    document.querySelectorAll('.room-type').forEach(room =>
-        room.classList.remove('selected-room'));
-    myTarget.classList.add('selected-room');
+    document
+        .querySelectorAll(".room-type")
+        .forEach((room) => room.classList.remove("selected-room"));
+    myTarget.classList.add("selected-room");
 }
 
-document.querySelector('#search-next-btn').addEventListener('click', (e) => findRoom(e));
+document
+    .querySelector("#search-next-btn")
+    .addEventListener("click", (e) => findRoom(e));
 
 function findRoom(e) {
     e.preventDefault();
-    const roomInfo = e.target.parentElement.parentElement.querySelector('.selected-room h4').textContent;
+    const roomInfo =
+        e.target.parentElement.parentElement.querySelector(
+            ".selected-room h4",
+        ).textContent;
     reservation.roomType = roomInfo;
     console.log(reservation);
-    changeContent('guest-details-form-content');
-
-document
-    .querySelector("#confirm-back-btn")
-    .addEventListener("click", (e) => getBackToPersonalData(e));
-
-function getBackToPersonalData(e) {
-    e.preventDefault();
     changeContent("guest-details-form-content");
-}
 
-document
-    .querySelector("#confirm-reservation")
-    .addEventListener("click", (e) => showThanksPage(e));
+    document
+        .querySelector("#confirm-back-btn")
+        .addEventListener("click", (e) => getBackToPersonalData(e));
 
-function showThanksPage(e) {
-    e.preventDefault();
-    changeContent("thank-you-content");
+    function getBackToPersonalData(e) {
+        e.preventDefault();
+        changeContent("guest-details-form-content");
+    }
+
+    document
+        .querySelector("#confirm-reservation")
+        .addEventListener("click", (e) => showThanksPage(e));
+
+    function showThanksPage(e) {
+        e.preventDefault();
+        changeContent("thank-you-content");
+    }
 }
